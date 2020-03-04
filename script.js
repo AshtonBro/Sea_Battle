d13d49cadecd571f2b331ea765911621470f8125
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-03-04 20:01:58 +0500

    get all id

---
+++ script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const record = document.getElementById('record'),
+    shot = document.getElementById('shot'),
+    hit = document.getElementById('hit'),
+    dead = document.getElementById('dead'),
+    enemy = document.getElementById('enemy'),
+    again = document.getElementById('again');
+
+const dataGame = {
+    record: 0,
+    shot: 0,
+    hit: 0,
+    dead: 0
+};
+
+const init = () => {
+    
+};
